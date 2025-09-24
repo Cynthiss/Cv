0ec749cd32fd76de19ad0c6c9737b038645e76f8
Michelle Santos <125904497+Cynthiss@users.noreply.github.com>
2025-09-23 20:09:43 -0600

    Agregar componente Experiencia
    Sección de experiencia laboral con tabla de periodo, empresa y puesto, incluyendo estilos CSS adaptados a Angular

--- cv-angular/src/app/experiencia/experiencia.ts
+++ cv-angular/src/app/experiencia/experiencia.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-experiencia',
-  imports: [],
-  templateUrl: './experiencia.html',
-  styleUrl: './experiencia.css'
+  templateUrl: './experiencia.component.html',
+  styleUrls: ['./experiencia.component.css']
 })
-export class Experiencia {
-
-}
+export class ExperienciaComponent { }
