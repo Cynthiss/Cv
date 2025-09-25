d309528c3528eda74172b15d96eb4bf9142fc2ac
Michelle Santos <125904497+Cynthiss@users.noreply.github.com>
2025-09-25 10:48:45 -0600

    Eliminación de app
    Elimininacion de los elementos para crearlos de nuevo por problemas de dependencias

--- cv-angular/src/app/perfil/perfil.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-perfil',
-  templateUrl: './perfil.html',
-  styleUrls: ['./perfil.css']
-})
-export class PerfilComponent {}
--- cv-angular/src/app/skills/skills.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-skills',
-  templateUrl: './skills.html',
-  styleUrls: ['./skills.css']
-})
-export class SkillsComponent { }
