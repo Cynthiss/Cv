b6650d85a89e0240113de97edcc09b83f4f04149
Michelle Santos <125904497+Cynthiss@users.noreply.github.com>
2025-10-14 22:05:31 -0600

    Component
    Cambios en app component, para evitar errores a la hora de compilar

--- cv-angular/src/app/app.component.ts
+++ cv-angular/src/app/app.component.ts
@@ -1,27 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';  // Necesario para los componentes standalone
+import { RouterModule } from '@angular/router';  // Asegúrate de importar RouterModule aquí
+import { AppRoutingModule } from './app-routing.module';  // Importa AppRoutingModule
+
 import { HeaderComponent } from './header/header.component';
 import { PerfilComponent } from './perfil/perfil.component';
 import { SkillsComponent } from './skills/skills.component';
 import { EducacionComponent } from './educacion/educacion.component';
 import { ExperienciaComponent } from './experiencia/experiencia.component';
 import { ProyectosComponent } from './proyectos/proyectos.component';
-import { MotivacionComponent } from './motivacion/motivacion.component';  // Importa MotivacionComponent
-import { CommonModule } from '@angular/common';  // Necesario para standalone components
-import { AppRoutingModule } from './app-routing.module';  // Importa el módulo de rutas
+import { MotivacionComponent } from './motivacion/motivacion.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    CommonModule,  // Necesario para los componentes standalone
+    CommonModule,
     HeaderComponent,
     PerfilComponent,
     SkillsComponent,
     EducacionComponent,
     ExperienciaComponent,
-    ProyectosComponent,  // Importa ProyectosComponent
-    MotivacionComponent,  // Importa MotivacionComponent
-    AppRoutingModule  // Asegúrate de incluir el módulo de rutas
+    ProyectosComponent,
+    MotivacionComponent,
+    AppRoutingModule  // Asegúrate de que AppRoutingModule esté importado
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
